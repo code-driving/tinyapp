dbe2ef1e12d4ab8f2021adb1817dfe63cd19dbfc
code-driving <tittan7@gmail.com>
2021-03-21 07:59:51 +0000

    Update the authUserByEmailAndPassword

--- helpers.js
+++ helpers.js
@@ -5,16 +5,29 @@ const { users } = require("./constants");
 const generateRandomString = (urlLength) => {
   let result = "";
   let characters =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   for (let i = 0; i < urlLength; i++) {
     result += characters.charAt(
       Math.floor(Math.random() * characters.length + 1)
-    );
-  }
-  return result;
-};
-
-const getUserByEmail = (email, database) => {
+      );
+    }
+    return result;
+  };
+  
+  const createUser = (email, password) => {
+    //generate random ID for new user
+    const id = generateRandomString(10);
+    const newUser = {
+      id,
+      email,
+      password: bcrypt.hashSync(password, saltRounds)
+    };
+    //add new user to a database
+    users[id] = newUser;
+    return newUser;
+  };
+  
+  const getUserByEmail = (email, database) => {
   //loop through the users object and check if the email provided in the form corresponds to the email in the database
   for (let userId in database) {
     const currentUser = database[userId];
@@ -30,24 +43,12 @@ const authUserByEmailAndPassword = (email, password) => {
   //check if the user exists &&
   //check if the the password of the found user in the database corresponds to the password provided in the form
   const currentUser = getUserByEmail(email, users);
-  if (currentUser && currentUser.password === password) {
+  if (currentUser &&  bcrypt.compareSync(password, currentUser.password)) {
     return currentUser;
   }
   return false;
 };
 
-const createUser = (email, password) => {
-  //generate random ID for new user
-  const id = generateRandomString(10);
-  const newUser = {
-    id,
-    email,
-    password: bcrypt.hashSync(password, saltRounds)
-  };
-  //add new user to a database
-  users[id] = newUser;
-  return newUser;
-};
 
 //return the URLs where the userID is equal to the id of the currently logged-in user
 const urlsForUser = (id, database) => {
@@ -65,11 +66,22 @@ const verifyID = (userID, shortURL, database) => {
   return userID === database[shortURL].userID;
 };
 
+//check if username is available
+const isAvailable = (newData, database) => {
+  for (let user in database) {
+    if (database[user].email === newData) {
+      return false;
+    }
+  }
+  return true;
+}
+
 module.exports = {
   generateRandomString,
   getUserByEmail,
   authUserByEmailAndPassword,
   createUser,
   urlsForUser,
-  verifyID
+  verifyID,
+  isAvailable
 };
